a2904d1c6238a7c8b9d19940b008ff75c6d84167
Komalpreet kaur <komalsidhu1103@gmail.com>
2023-11-24 10:18:16 -0500

    version1

--- Program.fs
+++ Program.fs
@@ -1,5 +1,5 @@
 ﻿type Coach = {
-    Name: String
+    Name: string
     FormerPlayer: bool
 }
 
@@ -9,17 +9,23 @@ type stats = {
 }
 
 type Team = {
-    Name: String
+    Name: string
     Coach: Coach
-    Stats: stats
+    stats: stats
 }
 
 
-// Create a list of 5 teams 
-let teams = [
-    {Name = "Chicago Bulls"; Coach = {Name = "Billy Donovan"; FormerPlayer= true} ; stats = {Wins=2344 ;Losses=2254 }}
-    {Name = "Indiana Pacers"; Coach = {Name = "Rick Carlisle"; FormerPlayer= true} ; stats = {Wins=1883 ;Losses=1903 }}
-    {Name = "Toronto Raptors"; Coach = {Name = "Darko Rajakovic"; FormerPlayer= true} ; stats = {Wins=1071 ;Losses=1157 }}
-    {Name = "Brooklyn Nets"; Coach = {Name = "Jacque Vaughn"; FormerPlayer= true} ; stats = {Wins=1622 ;Losses=2164 }}
-    {Name = "Atlanta Hawks"; Coach = {Name = "Quin Snyder"; FormerPlayer= true} ; stats = {Wins= 2891 ;Losses=2964 }}
+let tm = [
+    {Name = "Chicago Bulls"; Coach = {Name = "Billy Donovan"; FormerPlayer= true } ; stats = { Wins= 2344;Losses= 2254 }}
+    {Name = "Indiana Pacers"; Coach = {Name = "Rick Carlisle"; FormerPlayer= true } ; stats = { Wins= 1883;Losses= 1903 }}
+    {Name = "Toronto Raptors"; Coach = {Name = "Darko Rajakovic"; FormerPlayer= true } ; stats = { Wins= 1071;Losses= 1157 }}
+    {Name = "Brooklyn Nets"; Coach = {Name = "Jacque Vaughn"; FormerPlayer= true } ; stats = { Wins= 1622;Losses= 2164 }}
+    {Name = "Atlanta Hawks"; Coach = {Name = "Quin Snyder"; FormerPlayer= true } ; stats = { Wins= 2891;Losses= 2964}}
 ]
+
+let topteam = tm |> List.filter (fun tm -> tm.stats.Wins > tm.stats.Losses)
+
+//printfn "%A" topteam
+
+topteam |> List.iter (fun tm -> printfn "tm Name: %s" tm.Name)
+topteam |> List.iter (fun tm -> printfn "tm wins: %d" tm.stats.Wins)
